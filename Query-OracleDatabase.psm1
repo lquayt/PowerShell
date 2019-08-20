8d5a802e9bd26f5f749c02afe8ab4affd2077007
lquayt <lquaythevenon@gmail.com>
2019-08-20 23:08:48 +0200

    Update Query-OracleDatabase.psm1

--- Query-OracleDatabase.psm1
+++ Query-OracleDatabase.psm1
@@ -35,18 +35,18 @@
 Add-Type -Path "C:\Program Files\PackageManagement\NuGet\Packages\Oracle.ManagedDataAccess.12.1.24160719\lib\net40\Oracle.ManagedDataAccess.dll"
 
 Function Query-OracleDatabase{
-Param
-(
-[parameter(Mandatory=$true)] [String[]] $UserName,
-[parameter(Mandatory=$true)] [String[]] $Password,
-[parameter(Mandatory=$true)] [String[]] $Query,
-[parameter(Mandatory=$true)] [String[]] $Server,
-[String[]] $InstanceName = "",
-[Int32] $Port = 1521
-)
+    Param
+    (
+    [parameter(Mandatory=$true)] [String[]] $UserName,
+    [parameter(Mandatory=$true)] [String[]] $Password,
+    [parameter(Mandatory=$true)] [String[]] $Query,
+    [parameter(Mandatory=$true)] [String[]] $Server,
+    [String[]] $InstanceName = "",
+    [Int32] $Port = 1521
+    )
 
 
-$Connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection("User Id=$UserName;Password=$Password;Data Source=localhost/XE")
+    $Connection = New-Object Oracle.ManagedDataAccess.Client.OracleConnection("User Id=$UserName;Password=$Password;Data Source=localhost/XE")
 
 }
 
